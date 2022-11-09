ae24fd5854b22b4d210b7a2f5ef368ad264355b9
gubarv <114144472+gubarv@users.noreply.github.com>
2022-11-09 10:13:28 +0300

    Update работа колекции.py

--- работа колекции.py
+++ работа колекции.py
@@ -1,60 +1,14 @@
-# coding=utf-8
 from collections import namedtuple
 from collections import Counter
 
-
 class Biblioteka():
     def __init__(self, chapter, book_in, newspapers_in, journal_in):
-        if isinstance(chapter, str):
-            self.chapter = chapter
-        else:
-            raise InvalidNameError()
-
+        self.chapter = chapter
         self.book_in = book_in
         self.newspapers_in = newspapers_in
         self.journal_in = journal_in
 
-    def __add__(self, zavoz):
-        self.book_in += zavoz
-
-    def __call__(self, zavoz):
-        self.book_in = zavoz
-
-    def Monitoring(self):
-        print("Количество книг" + self.book_in)
-
-    def nn(self):
-        self.__number_of_newspapers_in_chapter()
-        self.__number_of_journal_in_chapter()
-
-    def __number_of_newspapers_in_chapter(self):  # кол-во газет в отделе с приватным доступом
-        print("В отделе " + self.chapter + " находится " + str(self.newspapers_in) + " газет.")
-
-    def __number_of_journal_in_chapter(self):  # кол-во журналов в отделе с приватным доступом
-        print("В отделе " + self.chapter + " находится " + str(self.journal_in) + " журналов.")
-
-
-class InvalidNameError(Exception):
-    def __str__(self):
-        return ("Неправильное название! Название состоит из цифр, а должно из букв! Проверка")
-
-
-try:
-    b1 = Biblioteka("Психология", 115, 22, 34)
-    # b1.nn()
-    # b1.otdel()
-
-except InvalidNameError as e:
-    print(e)
-
-
-# .Monitoring()
-# b1(150)
-# .Monitoring()
-# b1 + 50
-# b1.Monitoring()
-
-# 4 различных класса связаных с библиотекой
+        
 class books_by_author(): #класс с данными про книги автора(сколько их и сколько в библиотеке)
     def __init__(self, author, book_in, book_in_bib):
         self.author = author
